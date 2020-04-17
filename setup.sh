246b09c49e5bda55cf70dcfe2859b659f2e4e7c1
Matt Morse <mmorse1217@gmail.com>
2020-04-17 01:28:13 -0400

    corrected symlink paths when executed from another directory.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+MY_PATH="`dirname \"$0\"`"
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
+
 echo "symlinking .bashrc..."
-ln -sf `pwd`/.bashrc ~/.bashrc
+ln -sf $MY_PATH/.bashrc ~/.bashrc
 
 echo "symlinking .bash_aliases..."
-ln -sf `pwd`/.bash_aliases ~/.bash_aliases
+ln -sf $MY_PATH/.bash_aliases ~/.bash_aliases
 
 echo "symlinking .inputrc..."
-ln -sf `pwd`/.inputrc ~/.inputrc
+ln -sf $MY_PATH/.inputrc ~/.inputrc
 
 echo "symlinking .vimrc..."
-ln -sf `pwd`/.vimrc ~/.vimrc
+ln -sf $MY_PATH/.vimrc ~/.vimrc
 
 echo "symlinking .spacemacs..."
-ln -sf `pwd`/.spacemacs ~/.spacemacs
+ln -sf $MY_PATH/.spacemacs ~/.spacemacs
 
 echo "symlinking .tmux.conf..."
-ln -sf `pwd`/.tmux.conf ~/.tmux.conf
+ln -sf $MY_PATH/.tmux.conf ~/.tmux.conf
 
 OS=$(uname -s)
 if [[ $OS == "Darwin" ]]; then
     echo "symlinking .bash_profile on macOS"
-    ln -sf `pwd`/.bash_profile ~/.bash_profile
+    ln -sf $MY_PATH/.bash_profile ~/.bash_profile
     source ~/.bash_profile
 else
     echo "skipping .bash_profile: not on macOS"
